2ffd9dbce839e91a4b0f256436ca09c1f87c2364
Chris Fraser <github@cfraser.sent.com>
2021-10-19 17:57:35 -0400

    Add Readme and Comments

---
+++ readme.md
@@ -0,0 +1,17 @@
+# SPOT NFT Generator
+
+## Install
+
+Git pull this repository.
+
+## Run
+
+`cargo run` in root folder. Terminal will output messages and a file called `art.svg` will be saved in the root folder.
+
+## Modify
+
+To change the number of dots, edit the `main.rs` file's `num_spots` integer variable. Please use a perfect square.
+
+Change the margin by editing the `margin` variable. Please use an f32 or cast as f32.
+
+Change the viewBox dimensions with `dimension` variable.
--- src/main.rs
+++ src/main.rs
@@ -10,16 +10,30 @@ fn rand_num() -> f32 {
 }
 
 fn main() {
+    // Edit the number of dots by changing the integer num_spots to another perfect square. If not a
+    // square it will complain.
     let num_spots = 16;
+    // Change the margin here. Must be an f32 (include the decimal 1.0 for example). Margin must be
+    // less than half of `dimension`.
+    let margin = 10.0;
+    // Change the dimension of the SVG, this modifies the `viewBox` of the SVG. I suggest leaving it
+    // alone for now.
+    let dimension = 100;
+
+    // Leave the rest alone for now.
     let mut spots_vec = Vec::new();
     for _ in 0..num_spots {
-        spots_vec.push(svg_builder::Spot{color:Srgb::new(rand_num(), rand_num(), rand_num()), pointer: false })
+        spots_vec.push(svg_builder::Spot{
+            color:Srgb::new(rand_num(), rand_num(), rand_num()), pointer: false
+        })
     }
     let spots = svg_builder::Spots { spots: spots_vec };
 
-    let output = svg_builder::create(spots, 100, 10.0);
+    let output = svg_builder::create(spots, dimension, margin);
     match output {
-        Ok(()) => println!("SVG is available in root folder. Thank you very much, have a nice day. <3"),
+        Ok(()) => println!(
+            "SVG is available in root folder. Thank you very much, have a nice day. <3"
+        ),
         Err(e) => println!("Uh oh, something went wrong: {}", e),
     }
 }
